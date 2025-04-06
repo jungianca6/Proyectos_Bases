4243c06971015069918eae633e9d94bc25e8aae2
Brayan Alpizar <130131491+SpaceBa13@users.noreply.github.com>
2025-04-05 19:24:53 -0600

    Agregar archivos de proyecto.

---
+++ TECBank_BackEnd/Controllers/PagoContoller.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TECBank_BackEnd.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PagoContoller : ControllerBase
+    {
+    }
+}
---
+++ TECBank_BackEnd/Controllers/WeatherForecastController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TECBank_BackEnd.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+    }
+}
