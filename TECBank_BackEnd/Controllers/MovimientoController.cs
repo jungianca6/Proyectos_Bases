bcf746fc179ded2bc234d98b3fa72c7a8e1ecc07
Brayan Alpizar <130131491+SpaceBa13@users.noreply.github.com>
2025-04-07 22:50:09 -0600

    cambios en la estructura para el recibimiento de datos en el Backend

---
+++ TECBank_BackEnd/Controllers/MovimientoController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TECBank_BackEnd.Models;
+
+namespace TECBank_BackEnd.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovimientoController : ControllerBase
+    {
+        // POST: Movimiento/Pago
+        [HttpPost("Pago")]
+        public ActionResult Pago([FromBody] MovimientoModel data)
+        {
+            // Lógica para obtener datos
+            return Ok();
+        }
+
+        // POST: Movimiento/Transferencia
+        [HttpPost("Transferencia")]
+        public ActionResult Transferencia([FromBody] MovimientoModel data)
+        {
+            // Lógica para obtener datos
+            return Ok();
+        }
+
+        // POST: Movimiento/Retiro
+        [HttpPost("Retiro")]
+        public ActionResult Retiro([FromBody] MovimientoModel data)
+        {
+            // Lógica para obtener datos
+            return Ok();
+        }
+
+        // POST: Movimiento/Deposito
+        [HttpPost("Deposito")]
+        public ActionResult Deposito([FromBody] MovimientoModel data)
+        {
+            // Lógica para obtener datos
+            return Ok();
+        }
+
+    }
+}
