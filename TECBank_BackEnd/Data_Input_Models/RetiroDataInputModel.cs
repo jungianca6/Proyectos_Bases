90bf9a81d0a91bf98115f4442893596a0aeffb00
Brayan Alpizar <130131491+SpaceBa13@users.noreply.github.com>
2025-04-12 04:31:46 -0600

    Se agrego la funcionalidad de retiro

--- TECBank_BackEnd/Data_Input_Models/RetiroDataInputModel.cs
+++ TECBank_BackEnd/Data_Input_Models/RetiroDataInputModel.cs
@@ -6,11 +6,9 @@ namespace TECBank_BackEnd.Data_Input_Models
     {
         public string Nombre { get; set; }
 
-        public string NumeroDeCuenta { get; set; }
-
         public string Apellido1 { get; set; }
         public string Apellido2 { get; set; }
-        public float Monto { get; set; }
+        public int Monto { get; set; }
         public string ID { get; set; }
         public string Moneda { get; set; }
         public string CuentaARetirar { get; set; }
