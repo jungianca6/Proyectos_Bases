410f953aa673f2d1359885953d1612bf1fdbbb70
Brayan Alpizar <130131491+SpaceBa13@users.noreply.github.com>
2025-04-11 22:51:42 -0600

    cambios en deposito

--- TECBank_BackEnd/Data_Input_Models/DepositoDataInputModel.cs
+++ TECBank_BackEnd/Data_Input_Models/DepositoDataInputModel.cs
@@ -2,19 +2,10 @@
 
 namespace TECBank_BackEnd.Data_Input_Models
 {
-    public class DepositoDataInputModel
+    public class DepositoDataInputModel : MovimientoModel
     {
-        public string Nombre { get; set; }
-
-        public string NumeroDeCuenta { get; set; }
-
-        public string Apellido1 { get; set; }
-        public string Apellido2 { get; set; }
-        public float Monto { get; set; }
-        public string ID { get; set; }
-        public string Moneda { get; set; }
-        public CuentaModel CuentaEmisora { get; set; }
-        public CuentaModel CuentaDestino { get; set; }
+        public string CuentaEmisora { get; set; }
+        public string CuentaDestino { get; set; }
 
     }
 }
