9709fcaf7a3b3abb63740b724f49862a83f3b686
Kokigv <jorgutierrez@estudiantec.cr>
2025-04-13 02:07:58 -0600

    CalendarioPagos

---
+++ TECBank_BackEnd/Models/CalendarioPago.cs
@@ -0,0 +1,12 @@
+﻿namespace TECBank_BackEnd.Models
+{
+    public class CalendarioPago
+    {
+        public int ID { get; set; }
+        public int ID_Prestamo { get; set; }
+        public DateTime FechaPago { get; set; }
+        public decimal MontoAPagar { get; set; }
+        public string Mes { get; set; }
+        public int Anio { get; set; }
+    }
+}
